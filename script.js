d71b9e609a5ce91ff13e1bc7e4d1059e95ca7244
Shashank Shukla <64839400+Shashank-GitHub-23@users.noreply.github.com>
2023-09-04 11:32:29 +0530

    Block movement

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let calculateHole = document.getElementById("hole");
+let hole = document.getElementById("hole");
 
+
+hole.addEventListener("animationiteration", holeMovement);
+
+function holeMovement(){
+    let calculatehole = -((Math.random()*350)+150);
+    let newTop = calculatehole + 'px';
+    hole.style.top = newTop;
+}
