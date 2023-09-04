7618c8c28a4afd8e43d10f613aa129b20e519f58
Shashank Shukla <64839400+Shashank-GitHub-23@users.noreply.github.com>
2023-09-04 13:48:32 +0530

    Game Over part

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let hole = document.getElementById("hole");
-
+let CurrentflyValue = 0;
 
 hole.addEventListener("animationiteration", holeMovement);
 
@@ -8,3 +8,20 @@ function holeMovement(){
     let newTop = calculatehole + 'px';
     hole.style.top = newTop;
 }
+
+let fall = setInterval(() => {
+    let fallValue = parseInt(window.getComputedStyle(bird).getPropertyValue('top'));
+    if (CurrentflyValue == 0) {
+        bird.style.top = (fallValue + 2) + "px"; 
+    }
+},10);
+
+let birdFly = () => {
+    CureentflyValue = 1;
+    let flyValue = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+    bird.style.top = (flyValue - 65) + 'px';
+
+    setTimeout( () => CurrentflyValue = 0, 100 );
+}
+
+window.addEventListener("keydown", birdFly);
